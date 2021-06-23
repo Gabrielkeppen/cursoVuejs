23fe3936962b9001bd11300d25f7e23393c92f46
gabrielkeppen <gabrielkeppen@gmail.com>
2021-06-23 17:54:23 -0300

    desafio nao finalizado

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -1,6 +1,20 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: ''
-    }
+        valor: '',
+        enterValor: '',
+        withEnter: ''
+    },
+    methods: {
+        showAlert() {
+            alert('EXIBINDO ALERTA')
+        },
+        getText(e) {           
+        },
+        afterEnter() {
+            if enterValor != null
+                enterValor = withEnter
+            
+        }
+    },
 })
