4c2c37299b789c7800d7d39798f413c2042d7717
gabrielkeppen <gabrielkeppen@gmail.com>
2021-08-02 16:56:02 -0300

    projeto final

--- projeto-01/app.js
+++ projeto-01/app.js
@@ -11,6 +11,7 @@ new Vue ({
         stuned: 0,
         firePlus: 0,
         monsterPlus: 0,
+        logs: [],
     },
     computed: {
         hasResult() {
@@ -43,20 +44,24 @@ new Vue ({
             return Math.round(value)
         },
 
-        hurt(min, max, plusMin, plusMax) {
+        hurt(min, max, plusMin, plusMax, source, target, cls) {
             const hurt = this.getRandom(min + plusMin, max + plusMax)
             this.monsterLife = Math.max(this.monsterLife - hurt, 0)
+            if(hurt != undefined) {
+                this.registerLog(`${source} atingiu ${target} com ${hurt}.`)
+            }
         },
 
-        getHurt(min, max, plusMax, plusMin) {
+        getHurt(min, max, plusMax, plusMin, source, target, hurt) {
             const hurt = this.getRandom(min + plusMin, max + plusMax)
             this.playerLife = Math.max(this.playerLife - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`)
         },
 
 
         // ataques
         warriorAttack() {
-            this.hurt(5, 12, 0, 0)
+            this.hurt(5, 12, 0, 0, 'Jogador', 'Monstro', 'player')
             this.stuned = 0
             if (this.furyBar < 100) {
                 this.furyBar = this.furyBar + 20
@@ -64,11 +69,10 @@ new Vue ({
                     this.furyBar = 100
                 }
             }
-            
         },
 
         warriorKick() {
-            this.hurt(4, 8, 0, 0)
+            this.hurt(4, 8, 0, 0, 'Jogador', 'Monstro', 'player')
             this.stuned = 1 
             this.angryBar = this.angryBar + 0,4
             this.monsterPlus = this.monsterPlus + 0,4
@@ -81,13 +85,13 @@ new Vue ({
         },
 
         warriorFury() {
-            this.hurt(12, 30, 0, 0)
+            this.hurt(12, 30, 0, 0, 'Jogador', 'Monstro', 'player')
             this.stuned = 0
             this.furyBar = 0
         },
 
         wizardFire() {
-            this.hurt(1, 10, this.firePlus, this.firePlus)
+            this.hurt(1, 10, this.firePlus, this.firePlus, 'Jogador', 'Monstro', 'player')
             this.manaBar = this.manaBar - 10
             if (this.firePlus < 21) {
                 this.firePlus = this.firePlus + 3
@@ -100,7 +104,7 @@ new Vue ({
 
         wizardHeal() {
             this.manaBar = this.manaBar - 50
-            if (this.playerLife <= 75) {
+            if (this.playerLife <= 70) {
                 this.playerLife = this.playerLife + 30
             } else {
                 this.playerLife = 100
@@ -110,7 +114,7 @@ new Vue ({
         monsterAttack() {
             if (this.monsterLife != 0) {
                 if (this.stuned == 0) {
-                    this.getHurt(5, 10, this.monsterPlus, this.monsterPlus)
+                    this.getHurt(5, 10, this.monsterPlus, this.monsterPlus, 'Monstro', 'Jogador', 'player')
                 }
             }
         },
@@ -127,6 +131,11 @@ new Vue ({
                 this.playerLife = 0
             }
         },
+
+        //logs
+        registerLog(text, cls) {
+            this.log.unshift({ text, cls })
+        }
     },
     watch: {
 
