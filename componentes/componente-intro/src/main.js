64af593899d0d080475569978b570cb760d3c453
gabrielkeppen <gabrielkeppen@gmail.com>
2021-08-24 16:08:47 -0300

    organizando pastas

--- componentes/componente-intro/src/main.js
+++ componentes/componente-intro/src/main.js
@@ -1,11 +1,4 @@
-import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import Contador from './components/contador'
 
-Vue.component('contador', Contador)
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).mount('#app')
