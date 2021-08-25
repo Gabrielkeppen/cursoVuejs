0a19a0db4541a5401203f5ccdabeb23f86907809
gabrielkeppen <gabrielkeppen@gmail.com>
2021-08-25 15:35:41 -0300

    projeto arrumado

--- componentes/componente-intro/src/main.js
+++ componentes/componente-intro/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import Contador from './contador.vue'
 
+Vue.config.productionTip = false
 Vue.component('app-contador', Contador)
 
 new Vue({
-    render: h => h(App),
+  render: h => h(App),
 }).$mount('#app')
