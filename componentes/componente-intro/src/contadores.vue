50dc5667e6c76f21d36c329f5fa55313097a9c7e
gabrielkeppen <gabrielkeppen@gmail.com>
2021-08-26 16:06:08 -0300

    componente local e global

---
+++ componentes/componente-intro/src/contadores.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contadores">
+        <app-contador/>
+    </div>
+</template>
+
+<script>
+
+import Contador from './contador.vue'
+
+export default {
+    components: {
+        'app-contador': {Contador}
+    }
+}
+</script>
+
+<style>
+
+</style>
