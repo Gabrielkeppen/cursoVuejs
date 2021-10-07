cd9c8f5b41ce6c5696b3aa639ca16d4ab3eab493
gabrielkeppen <gabrielkeppen@gmail.com>
2021-10-07 17:56:58 -0300

    novo desafio de comunicação de componentes

---
+++ comunicacao_desafio/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="md-layout">
+      <div class="md-layout-item md-size-50">
+        <PeopleList />
+      </div>
+      <div class="md-layout-item md-size-50">
+        <PeopleShow />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PeopleList from '../src/components/peopleList.vue'
+import PeopleShow from '../src/components/peopleShow.vue'
+
+export default {
+  components: {
+    PeopleList,
+    PeopleShow,
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.md-layout-item {
+  border: 2px;
+}
+
+</style>
---
+++ comunicacao_desafio/src/components/peopleList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="list">
+        <table class="list-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NOME</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="(item, i) in list" :key="i">
+                    <tr>
+                        <td>
+                            {{ item.id }}
+                        </td>
+                        <td>
+                            {{ item.name }}
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default ({
+    data() {
+        return {
+            list: [
+                {id:1, name:"Gabriel", age:"20"},
+                {id:2, name:"Pedro", age:"17"},
+                {id:3, name:"Arthur", age:"14"},
+                {id:4, name:"Patricia", age:"42"},
+                {id:5, name:"Silvio", age:"46"},
+            ]
+        }
+    }
+})
+</script>
