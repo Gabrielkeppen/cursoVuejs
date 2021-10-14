71df2650eb632b8726a37566aa7c828734826165
gabrielkeppen <gabrielkeppen@gmail.com>
2021-10-14 17:21:38 -0300

    iniciando desafio comp avancado

--- citacoes/src/components/Citacao.vue
+++ citacoes/src/components/Citacao.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="citacao">
-        
+        <slot name="autor"></slot>
+        <slot></slot>
+        <slot name="fonte"></slot>
     </div>
 </template>
 
 <script>
-export default {
-
+export default { 
+    created() {
+        console.log('created')
+    },
+    destroyed() {
+        console.log('destroyed')
+    },
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    }
 }
 </script>
 
