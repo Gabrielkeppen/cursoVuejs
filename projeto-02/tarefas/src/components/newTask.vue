144c84bf4dce3b11a0ec309c7d9577f38ae0ce25
gabrielkeppen <gabrielkeppen@gmail.com>
2022-02-24 16:44:36 -0300

    novo projeto

---
+++ projeto-02/tarefas/src/components/newTask.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="new-task">
+        <input type="text">
+        <button>+</button>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
---
+++ projeto-02/tarefas/src/components/taskGrid.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="tasks-grid">
+        <template v-if="tasks.length">
+            <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+        </template>
+        <p v-else class="no-task">Sua vida está em dia.</p>
+    </div>
+</template>
+
+<script>
+    import Task from './task.vue'
+
+    export default {
+        components: {
+            Task,
+        },
+        props: {
+            tasks: { type: Array, required: true }
+        }
+    }
+</script>
+
+<style>
+    .tasks-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .tasks-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #aaa;
+        font-size: 1.7rem;
+    }
+</style>
